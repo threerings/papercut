bfa3156a285ab9c40fcec45d5092c9da8c7c9d9e
Charlie Groves <charlie.groves@gmail.com>
2011-09-14 16:18:54 -0700

    Waffle back to using locals

--- project/Build.scala
+++ project/Build.scala
@@ -1,8 +1,25 @@
 import sbt._
 import Keys._
 
+// allows projects to be symlinked into the current directory for a direct dependency,
+// or fall back to obtaining the project from Maven otherwise
+class Locals (locals :(String, String, ModuleID)*) {
+  def addDeps (p :Project) = (locals collect {
+    case (id, subp, dep) if (file(id).exists) => symproj(file(id), subp)
+  }).foldLeft(p) { _ dependsOn _ }
+  def libDeps = locals collect {
+    case (id, subp, dep) if (!file(id).exists) => dep
+  }
+  private def symproj (dir :File, subproj :String = null) =
+    if (subproj == null) RootProject(dir) else ProjectRef(dir, subproj)
+}
+
 object PapercutBuild extends Build {
-  lazy val papercut = Project(
+  val locals = new Locals(
+    ("flashbang-playn", null, "com.threerings" % "flashbang-playn" % "1.0-SNAPSHOT")
+  )
+
+  lazy val papercut = locals.addDeps(Project(
     "papercut", file("."), settings = Defaults.defaultSettings ++ Seq(
       organization := "com.threerings",
       version      := "1.0-SNAPSHOT",
@@ -13,14 +30,7 @@ object PapercutBuild extends Build {
       fork in Compile := true,
 
       autoScalaLibrary := false, // no scala-library dependency
-      libraryDependencies ++= Seq(
-        "com.samskivert" % "pythagoras" % "1.1-SNAPSHOT",
-        "com.threerings" % "react" % "1.0-SNAPSHOT",
-        "com.googlecode.playn" % "playn-core" % "1.0-SNAPSHOT",
-        "com.googlecode.playn" % "playn-java" % "1.0-SNAPSHOT",
-        "com.threerings" % "tripleplay" % "1.0-SNAPSHOT",
-        "com.threerings" % "flashbang-playn" % "1.0-SNAPSHOT"
-      )
+      resolvers        += "Forplay Legacy" at "http://forplay.googlecode.com/svn/mavenrepo"
     )
-  )
+  ))
 }
