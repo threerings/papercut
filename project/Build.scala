3e9e7146dca78cd0506be097148f56299b85fc07
Charlie Groves <charlie.groves@gmail.com>
2011-09-13 17:18:55 -0700

    Depend on artifacts instead of projects
    Enough fiddling with the symlinks; can't get all the dependencies to
    line up.

--- project/Build.scala
+++ project/Build.scala
@@ -1,29 +1,8 @@
 import sbt._
 import Keys._
 
-// allows projects to be symlinked into the current directory for a direct dependency,
-// or fall back to obtaining the project from Maven otherwise
-class Local (locals :(String, String, ModuleID)*) {
-  def addDeps (p :Project) = (locals collect {
-    case (id, subp, dep) if (file(id).exists) => symproj(file("../" + id), subp)
-  }).foldLeft(p) { _ dependsOn _ }
-  def libDeps = locals collect {
-    case (id, subp, dep) if (!file(id).exists) => {
-      println("Using dep: " + dep)
-      dep
-    }
-  }
-  private def symproj (dir :File, subproj :String = null) =
-    if (subproj == null) RootProject(dir) else ProjectRef(dir, subproj)
-}
-
 object PapercutBuild extends Build {
-  val locals = new Local(
-    ("flashbang-playn", null, "com.threerings" % "flashbang-playn" % "1.0-SNAPSHOT")
-  )
-
-
-  lazy val papercut = locals.addDeps(Project(
+  lazy val papercut = Project(
     "papercut", file("."), settings = Defaults.defaultSettings ++ Seq(
       organization := "com.threerings",
       version      := "1.0-SNAPSHOT",
@@ -33,14 +12,15 @@ object PapercutBuild extends Build {
       javacOptions ++= Seq("-Xlint", "-Xlint:-serial"),
       fork in Compile := true,
 
-      resolvers += "Local Maven Repository" at Path.userHome.asURL + "/.m2/repository",
-
       autoScalaLibrary := false, // no scala-library dependency
-      libraryDependencies ++= locals.libDeps ++ Seq(
-        // test dependencies
-        "junit" % "junit" % "4.+" % "test",
- 	    "com.novocode" % "junit-interface" % "0.7" % "test->default"
+      libraryDependencies ++= Seq(
+        "com.samskivert" % "pythagoras" % "1.1-SNAPSHOT",
+        "com.threerings" % "react" % "1.0-SNAPSHOT",
+        "com.googlecode.playn" % "playn-core" % "1.0-SNAPSHOT",
+        "com.googlecode.playn" % "playn-java" % "1.0-SNAPSHOT",
+        "com.threerings" % "tripleplay" % "1.0-SNAPSHOT",
+        "com.threerings" % "flashbang-playn" % "1.0-SNAPSHOT"
       )
     )
-  ))
+  )
 }
