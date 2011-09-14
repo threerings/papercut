c26460ee282c9681f8c06b3d283533f73ae9b027
Charlie Groves <charlie.groves@gmail.com>
2011-09-14 16:20:36 -0700

    Highlight a selected button

--- src/main/java/papercut/AnimateMode.java
+++ src/main/java/papercut/AnimateMode.java
@@ -8,9 +8,12 @@ import playn.core.PlayN;
 import react.Slot;
 
 import tripleplay.ui.AxisLayout;
+import tripleplay.ui.Background;
 import tripleplay.ui.Button;
 import tripleplay.ui.Interface;
 import tripleplay.ui.Root;
+import tripleplay.ui.Style;
+import tripleplay.ui.Styles;
 
 import com.threerings.flashbang.AppMode;
 
@@ -23,16 +26,28 @@ public class AnimateMode extends AppMode
     @Override protected void setup () {
         super.setup();
 
+        final Styles selectedStyles = Styles.none().
+            add(Style.COLOR.is(0xFFFFFFFF)).
+            add(Style.BACKGROUND.is(Background.solid(0xFF000000)));
+
+        final Styles deselectedStyles = Styles.none().
+            add(Style.COLOR.is(0xFF000000)).
+            add(Style.BACKGROUND.is(Background.solid(0xFFFFFFFF)));
+
         _iface = new Interface(pointerListener());
         _root = _iface.createRoot(AxisLayout.vertical().alignLeft());
-        _modeLayer.add(_root.layer);
+        modeLayer.add(_root.layer);
         for (String asset : _assets) {
             if (!asset.endsWith(".png")) { continue; }
             Button adder = new Button().setText(asset);
             _root.add(adder);
             adder.click.connect(new Slot<Button> () {
                 @Override public void onEmit (Button b) {
-                    System.out.println("Clicked: " + b.text());
+                    if (_selected != null) {
+                        _selected.addStyles(deselectedStyles);
+                    }
+                    _selected = b;
+                    _selected.addStyles(selectedStyles);
                 }
             });
         }
@@ -48,5 +63,6 @@ public class AnimateMode extends AppMode
 
     protected Root _root;
     protected Interface _iface;
+    protected Button _selected;
     protected final Iterable<String> _assets;
 }
