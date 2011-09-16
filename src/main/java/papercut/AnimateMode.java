11ce80b660ab83841212d79e219c37db31d528b0
Charlie Groves <charlie.groves@gmail.com>
2011-09-15 19:54:38 -0700

    Use Selector

--- src/main/java/papercut/AnimateMode.java
+++ src/main/java/papercut/AnimateMode.java
@@ -12,8 +12,10 @@ import react.Slot;
 import tripleplay.ui.AxisLayout;
 import tripleplay.ui.Background;
 import tripleplay.ui.Button;
+import tripleplay.ui.Element;
 import tripleplay.ui.Interface;
 import tripleplay.ui.Root;
+import tripleplay.ui.Selector;
 import tripleplay.ui.Style;
 import tripleplay.ui.Styles;
 
@@ -29,41 +31,34 @@ public class AnimateMode extends AppMode
     @Override protected void setup () {
         super.setup();
 
-        final Styles selectedStyles = Styles.none().
-            add(Style.COLOR.is(0xFFFFFFFF), Style.BACKGROUND.is(Background.solid(0xFF000000, 2)));
-
-
-        final Styles deselectedStyles = Styles.none().
-            add(Style.COLOR.is(0xFF000000), Style.BACKGROUND.is(Background.solid(0xFFFFFFFF, 2)));
+        final Styles styles = Styles.
+            make(Style.BACKGROUND.is(Background.solid(0xFFFFFFFF, 2))).
+            makeSelected(
+                Style.COLOR.is(0xFFFFFFFF), Style.BACKGROUND.is(Background.solid(0xFF000000, 2)));
 
         _iface = new Interface(pointerListener());
         PlayN.pointer().setListener(_iface.plistener);
 
         _listing = _iface.createRoot(AxisLayout.vertical().alignLeft().gap(0));
         modeLayer.add(_listing.layer);
-        for (final String image : _images) {
-            Button adder = new Button(deselectedStyles).setText(image);
-            _listing.add(adder);
-            adder.click.connect(new Slot<Button> () {
-                @Override public void onEmit (Button b) {
-                    if (_selected != null) {
-                        _selected.addStyles(deselectedStyles);
-                        _image.destroy();
-                    }
-                    _selected = b;
-                    ImageResource rsrc = ImageResource.require(image);
-                    _image = PlayN.graphics().createImageLayer(rsrc.image());
-                    modeLayer.add(_image);
-
-                    _selected.addStyles(selectedStyles);
+        new Selector(_listing).selected.connect(new Slot<Element<?>>() {
+            @Override public void onEmit (Element<?> b) {
+                if (_image != null) {
+                    _image.destroy();
                 }
-            });
+                ImageResource rsrc = ImageResource.require(((Button)b).text());
+                _image = PlayN.graphics().createImageLayer(rsrc.image());
+                modeLayer.add(_image);
+            }
+        });
+        for (String image : _images) {
+            _listing.add(new Button(styles).setText(image));
         }
         _listing.packToWidth(200);
 
         PlayN.mouse().setListener(new Mouse.Adapter() {
             @Override public void onMouseMove (Mouse.MotionEvent ev) {
-                if (_selected == null) { return; }
+                if (_image == null) { return; }
                 _image.setTranslation(ev.x(), ev.y());
             }
         });
@@ -76,7 +71,6 @@ public class AnimateMode extends AppMode
 
     protected Root _listing, _stage;
     protected Interface _iface;
-    protected Button _selected;
     protected ImageLayer _image;
     protected final Iterable<String> _images;
 }
