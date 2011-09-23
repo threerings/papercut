097e675d291edd821fa5d306978c397a1ab85751
Charlie Groves <charlie.groves@gmail.com>
2011-09-23 16:57:08 -0700

    Add editors for all keyframe types

--- src/main/java/papercut/KeyframeEditor.java
+++ src/main/java/papercut/KeyframeEditor.java
@@ -3,6 +3,9 @@
 
 package papercut;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 import pythagoras.f.MathUtil;
 
 import react.Function;
@@ -15,6 +18,7 @@ import tripleplay.ui.Label;
 import tripleplay.ui.Slider;
 
 import flashbang.anim.rsrc.EditableLayerAnimation;
+import flashbang.anim.rsrc.KeyframeType;
 
 import static flashbang.anim.rsrc.KeyframeType.ROTATION;
 
@@ -22,28 +26,41 @@ public class KeyframeEditor extends Elements<KeyframeEditor>
 {
     public KeyframeEditor () {
         super(AxisLayout.vertical());
-        addSlider(_rotation, "Rotation");
-        _rotation.value.connect(new Slot<Float> () {
-            @Override public void onEmit (Float val) {
-                if (_updatingFrame) return;
-                _layer.add(ROTATION, _frame, (float)Math.toRadians(val));
-            }
-        });
+        for (final KeyframeType kt : KeyframeType.values()) {
+            Slider slider = createSlider(kt).setConstraint(AxisLayout.stretched());
+            Group row = new Group(AxisLayout.horizontal());
+            Label value = new Label();
+            add(new Group(AxisLayout.horizontal()).add(new Label(kt.displayName), slider, value));
+            slider.value.map(FORMAT).connectNotify(value.textSlot());
+            slider.value.connect(new Slot<Float> () {
+                @Override public void onEmit (Float val) {
+                    if (_updatingFrame) return;
+                    _layer.add(kt, _frame, val);
+                }
+            });
+        }
     }
 
-    protected void addSlider(Slider slider, String name) {
-        slider.setConstraint(AxisLayout.stretched());
-        Group row = new Group(AxisLayout.horizontal());
-        Label value;
-        add(new Group(AxisLayout.horizontal()).add(new Label(name), slider, value = new Label()));
-        slider.value.map(FORMAT).connectNotify(value.textSlot());
+    protected static Slider createSlider (KeyframeType kt) {
+        switch (kt) {
+            case X_LOCATION: return new Slider(0, -500, 500);
+            case Y_LOCATION: return new Slider(0, -500, 500);
+            case X_SCALE: return new Slider(1, 0, 10);
+            case Y_SCALE: return new Slider(1, 0, 10);
+            case ROTATION: return new Slider(0, 0, MathUtil.TWO_PI);// TAU CAN SUCK IT, GREENWELL
+            case ALPHA: return new Slider(1, 0, 1);
+            default: throw new RuntimeException("Unhandled keyframe type: " + kt);
+        }
     }
 
     public void setFrame (EditableLayerAnimation layer, int frame) {
         _updatingFrame = true;
         _frame = frame;
         _layer = layer;
-        _rotation.value.update(_layer.keyframes.get(ROTATION).find(frame).interp(frame));
+        for (Map.Entry<KeyframeType, Slider> entry : _sliders.entrySet()) {
+            float value = _layer.keyframes.get(entry.getKey()).find(frame).interp(frame);
+            entry.getValue().value.update(value);
+        }
         _updatingFrame = false;
     }
 
@@ -51,7 +68,7 @@ public class KeyframeEditor extends Elements<KeyframeEditor>
     protected EditableLayerAnimation _layer;
     protected boolean _updatingFrame;
 
-    protected final Slider _rotation = new Slider(0, -180, 180);
+    protected final Map<KeyframeType, Slider> _sliders = new EnumMap<KeyframeType, Slider>(KeyframeType.class);
 
     protected static Function<Float, String> FORMAT = new Function<Float, String>() {
         @Override public String apply (Float v) {
