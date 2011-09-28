e0d6b62d09e4352c8728c021c81a0ef1c3e146c7
Charlie Groves <charlie.groves@gmail.com>
2011-09-28 00:15:40 -0700

    Hook up layer remove, indent, and dedent

--- src/main/java/papercut/LayerEditor.java
+++ src/main/java/papercut/LayerEditor.java
@@ -11,6 +11,7 @@ import tripleplay.ui.Elements;
 
 import flashbang.anim.rsrc.EditableMovieConf;
 import flashbang.anim.rsrc.EditableMovieGroupLayerConf;
+import flashbang.anim.rsrc.EditableMovieLayerConf;
 import flashbang.anim.rsrc.MovieConf;
 
 public class LayerEditor extends Elements<LayerEditor>
@@ -21,6 +22,43 @@ public class LayerEditor extends Elements<LayerEditor>
         _add.clicked().connect(new UnitSlot() {
             @Override public void onEmit () {
                 conf.add(tree.groupLayer(), new EditableMovieGroupLayerConf("Group"));
+            }
+        });
+        _remove.clicked().connect(new UnitSlot() {
+            @Override public void onEmit () {
+                EditableMovieLayerConf toRemove = tree.layer();
+                if (toRemove == null) return;
+                conf.findParent(toRemove).children.remove(toRemove);
+            }
+        });
+        _indent.clicked().connect(new UnitSlot() {
+            @Override public void onEmit () {
+                EditableMovieLayerConf toIndent = tree.layer();
+                if (toIndent == null) return;
+                EditableMovieGroupLayerConf currentParent = conf.findParent(toIndent);
+                int idx = currentParent.children.indexOf(toIndent);
+                currentParent.children.remove(idx);
+                if (idx != 0) {
+                     EditableMovieLayerConf prevSib =  currentParent.children.get(idx - 1);
+                     if (prevSib instanceof EditableMovieGroupLayerConf) {
+                         ((EditableMovieGroupLayerConf)prevSib).children.add(toIndent);
+                         return;
+                     }
+                }
+                EditableMovieGroupLayerConf newParent = new EditableMovieGroupLayerConf("Group");
+                conf.add(currentParent, newParent, idx);
+                newParent.children.add(toIndent);
+            }
+        });
+        _dedent.clicked().connect(new UnitSlot() {
+            @Override public void onEmit () {
+                EditableMovieLayerConf toDedent = tree.layer();
+                if (toDedent == null) return;
+                EditableMovieGroupLayerConf currentParent = conf.findParent(toDedent);
+                if (currentParent == conf.root) return;
+                currentParent.children.remove(toDedent);
+                EditableMovieGroupLayerConf newParent = conf.findParent(currentParent);
+                newParent.children.add(newParent.children.indexOf(currentParent) + 1, toDedent);
             }
         });
     }
