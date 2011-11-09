f396652c3cbcca44c52ef779da414e70e5fc8d8b
Michael Bayne <mdb@samskivert.com>
2011-11-09 13:53:15 -0800

    Use the latest Scala (2.9.1).

--- project/Build.scala
+++ project/Build.scala
@@ -11,12 +11,12 @@ object PapercutBuild extends Build {
       organization := "com.threerings",
       version      := "1.0-SNAPSHOT",
       name         := "papercut",
-      scalaVersion := "2.9.0-1",
+      scalaVersion := "2.9.1",
       scalacOptions ++= Seq("-deprecation"),
       javacOptions ++= Seq("-Xlint", "-Xlint:-serial"),
       fork in Compile := true,
       libraryDependencies ++= Seq(
-        "org.scala-lang" % "scala-swing" % "2.9.0-1"
+        "org.scala-lang" % "scala-swing" % "2.9.1"
       )
     )
   ))
